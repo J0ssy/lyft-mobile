049591f0081b6122d08b48f967ea9e472ea89957
J0ssy <j.g.basterrica@gmail.com>
2017-12-14 11:33:51 -0300

    fix functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,6 @@
+//Variable global del código generado luego de ingresar números al input
 var code = "";
+var otherCode = "";
 
 $(document).ready(function(){
   //Cargador Inicial
@@ -22,14 +24,14 @@ $(document).ready(function(){
   }
 
   //Volver a login
-  $('.signup-section span:first').click(backToSignUp);
-  function backToSignUp() {
+  $('.signup-section span:first').click(backToLogin);
+  function backToLogin() {
     $('.signup-section').hide();
     $('.login-section').show();
   }
 
    //Validar 10 números, sólo números y activar botón
-  $("#inputNumbers").keyup(function() {
+  $('#inputNumbers').keyup(function() {
     var onlyNumbers = /^[0-9]+$/;
     var inputNumbers = $(this).val();
       if ( inputNumbers.length >= 10 && onlyNumbers.test(inputNumbers)) {
@@ -38,18 +40,36 @@ $(document).ready(function(){
         $('.btn-next').addClass('disabled'); {
 
         }
-    })
+    });
 
   //Generar código
   $('.btn-next').click(generateCode);
   function generateCode() {
-  var str = $("#inputNumbers").val();
+  var code = "";
+  var str = $('#inputNumbers').val();
   for (var i = 0; i < 3; i++) {
     code += str.charAt(Math.floor(Math.random() * str.length));
     }
-     alert(code);
+    alert(code);
+    $('.signup-section').hide();
+    $('.verify-section').show();
   }
 
-
+  //Volver a Sign Up
+  $('.verify-section span:first').click(backToSignUp);
+  function backToSignUp() {
+    $('.verify-section').hide();
+    $('.signup-section').show();
+  }
+  //Re-generar código
+  $('.btn-resend').click(resendCode);
+  function resendCode() {
+  otherCode = "";
+  var str = $('#inputNumbers').val();
+  for (var i = 0; i < 3; i++) {
+    otherCode += str.charAt(Math.floor(Math.random() * str.length));
+    }
+    alert(otherCode);
+  }
 
 });
