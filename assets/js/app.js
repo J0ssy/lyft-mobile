a8b19dd3e9193a152066e01f2602d1e535d96dee
J0ssy <j.g.basterrica@gmail.com>
2017-12-14 10:11:12 -0300

    verificador de números listo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,47 @@
 $(document).ready(function(){
   //Cargador Inicial
-  setInterval(function() {
+  setTimeout(function() {
     $('.load-section').fadeOut();
     $('.login-section').show();
   }, 2000);
 
+  //Regresar al Loading
+  $('.login-section span:first').click(backToLoading);
+    function backToLoading() {
+    $('.login-section').fadeOut();
+    $('.load-section').show();
+  }
+
+  //Avanzar con el botón Sign Up
+  $('.login-section button:last-child').click(nextToSignUp);
+  function nextToSignUp() {
+    $('.login-section').hide();
+    $('.signup-section').show();
+  }
+
+  //Volver a login
+  $('.signup-section span:first').click(backToSignUp);
+  function backToSignUp() {
+    $('.signup-section').hide();
+    $('.login-section').show();
+  }
+
+   //Validar 10 números, sólo números y activar botón
+  $("#inputNumbers").keyup(function() {
+    var onlyNumbers = /^[0-9]+$/;
+    var inputNumbers = $(this).val();
+      if ( inputNumbers.length >= 10 && onlyNumbers.test(inputNumbers)) {
+        $('.btn-next').removeClass('disabled');
+      } else
+        $('.btn-next').addClass('disabled'); {
+
+        }
+    })
+
+  //Avanzar a Code Section
+  $('.btn-next').click(nextToCodeSection);
+  function nextToCodeSection() {
+ 
+  }
 
 });
